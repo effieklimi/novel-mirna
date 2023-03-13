101149645155458a7b081fd3f48b6ae2b6e0d3f6
effieklimi <effieklimi@gmail.com>
2023-03-13 23:37:16 +0000

    finished vsmc GSEA

--- code/GSEA.R
+++ code/GSEA.R
@@ -5,9 +5,18 @@ library(fgsea)
 library(plyr)
 library(grid)
 
+options(warn = 1)
 setwd("/Users/effieklimi/Documents/novel-mirna/")
 
 ########## pathways ##########
+read.geneset <- function(path_to_gset){
+  bp = GSA.read.gmt(path_to_gset)
+  out = bp$genesets
+  out = lapply(1:length(out), function(x) out[[x]][out[[x]] != ''])
+  names(out) = bp$geneset.names
+  return(out)
+}
+
 bp <- read.geneset(url("https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=GO_Biological_Process_2021"))
 ke <- read.geneset(url("https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=KEGG_2021_Human"))
 re <- read.geneset(url("https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=Reactome_2022"))
@@ -15,35 +24,20 @@ all_paths <- c(bp, ke, re)
 names(all_paths) <- tolower(names(all_paths))
 ##############################
 
-############## files ##############
-# Open files containg gene and DE info per miRNA -> put them all inside a list:
-deseqFiles <-
-  list.files("/deseqResults/diffExprTables",
-    pattern = "*.csv", full.names = TRUE) %>%
-  lapply(read_csv) %>%
-  lapply(distinct, name, .keep_all = TRUE)
+shrinkResults <- 
+  readRDS("results/tables/vsmc-deseq2-results.rds") %>%
+  lapply(distinct, name, .keep_all = TRUE) # remove duplicates
 
-# Name tibbles based on their filenames (miRNA name)
-names(deseqFiles) <-
-  list.files("/deseqResults/diffExprTables",
-    pattern = "*.csv", full.names = FALSE) %>%
-  lapply(gsub, pattern = "vsmiRCTRL_noLFCthreshold.csv", replacement = "")
-# Save list of dfs containing all DE genes (without duplicates):
-saveRDS(deseqFiles, file = "/deseqResults/deseqResultsTibble.rds")
-####################################
-
-# Create gene lists with LFC and gene symbols as names, ranked from high LFC to low
 geneLists <-
-  lapply(deseqFiles, "[", , c(8, 4)) %>%
+  lapply(shrinkResults, "[", , c(7, 3)) %>%
   lapply(tibble::deframe) %>%
   lapply(sort, decreasing = TRUE)
 # Save list of DE genes per miRNA with LFC (it's ranked):
-saveRDS(geneLists, file = "/Users/effieklimi/Documents/PhD/miRNA screening paper/HSVSMC RNA sequencing/GSEA/NoLFCthresh/geneLists.rds")
+saveRDS(geneLists, file = "results/tables/geneLists.rds")
 
-# Perform GSEA
 fgseaResults <-
-  map(geneLists, fgsea, pathways = all_paths, minSize = 20, maxSize = 1000, eps = 0) %>%
-  saveRDS(fgseaResults, file = "/Users/effieklimi/Documents/PhD/miRNA screening paper/HSVSMC RNA sequencing/GSEA/NoLFCthresh/fgseaResults.rds")
+  map(geneLists, fgsea, pathways = all_paths, minSize = 20, maxSize = 1000, eps = 0)
+saveRDS(fgseaResults, file = "results/tables/vsmc-fgsea.rds")
 
 # Filter for p value, keep the Normalised Enrichment Scores and format table for the heatmap:
 fgseaResultsSig <-
@@ -56,8 +50,8 @@ fgseaResultsSig <-
     as.character(pathway) %in% tolower(names(bp)) ~ "Gene Ontology BP"
   )) %>%
   column_to_rownames(var = "pathway") %>%
-  `colnames<-`(c(names(deseqFiles), "Database"))
+  `colnames<-`(c(names(geneLists), "Database"))
 
 #### Save table of results for heatmap: ####
-write.csv(fgseaResultsSig, file = "/Users/effieklimi/Documents/PhD/miRNA screening paper/HSVSMC RNA sequencing/GSEA/NoLFCthresh/fgseaResultsSigNES.csv")
+write.csv(fgseaResultsSig, file = "results/tables/fgsea-results-sigNES.csv")
 ############################################
